7c331e15cb6c3d134fa86953490bd0472dc6784b
yuliana-shakhvalieva <shahvalieva.yuliana@yandex.ru>
2023-11-24 00:46:31 +0300

    Make code style changes in homework 6

--- Homework_6/src/lazy_string.hpp
+++ Homework_6/src/lazy_string.hpp
@@ -3,89 +3,106 @@
 #include <iostream>
 
 namespace std_utils {
-
-namespace details {
-} // namespace details
-
-	template <class T, class S>
+	template <class CharT, class Traits>
 	class lazy_basic_string;
 
-	template <class T, class S>
-	bool operator== (const T * left, const lazy_basic_string<T, S> & right) {
-		return (right == left);
-	}
-
-	template <class T, class S>
-	bool operator< (const T * left, const lazy_basic_string<T, S> & right) {
-		return !(right < left);
-	}
-
-	template <class T, class S>
-	bool operator!= (const T * left, const lazy_basic_string<T, S> & right) {
-		return right != left;
-	}
-
-	template <class T, class S>
-	lazy_basic_string<T, S> operator+ (const T * left, const lazy_basic_string<T, S> & right) {
-		lazy_basic_string<T, S> result;
-		result += left;
-		result += right;
-		return result;
-	}
-
-	template <class T, class S>
-	lazy_basic_string<T, S> operator+(const T left, const lazy_basic_string<T, S> & right) {
-		lazy_basic_string<T, S> result;
-		result += left;
-		result += right;
-		return result;
-	}
-
-	template <class T, class S>
-	void swap(lazy_basic_string<T, S> & left, lazy_basic_string<T, S> & right) {
-		left.swap(right);
-	}
-
-	template <class CharT, class Traits = std::char_traits<CharT>>
-	class Proxy {
-	public:
-		Proxy(lazy_basic_string <CharT, Traits> * input_string, size_t idx)
-			:lazy_string_ptr_(input_string) , position_(idx)
-		{ }
-
-		operator CharT() {
-			return *(lazy_string_ptr_->c_str() + position_);
+	namespace details {
+		template <class CharT, class Traits>
+		bool operator== (CharT const *left, lazy_basic_string<CharT, Traits> const &right) {
+			return right == left;
 		}
 
-		Proxy& operator= (const CharT & value) {
-			size_t size = lazy_string_ptr_->size();
-
-			std::shared_ptr<CharT[]> new_data = std::make_unique<CharT[]>(size + 1);
-
-			Traits::copy(new_data.get(), lazy_string_ptr_->c_str(), size);
-
-			*(new_data.get() + position_) = value;
-			lazy_string_ptr_->set_data(new_data);
-
-			return *this;
+		template <class CharT, class Traits>
+		bool operator< (CharT const *left, lazy_basic_string<CharT, Traits> const &right) {
+			return !(right < left) && (right != left);
 		}
 
-	private:
-		lazy_basic_string<CharT, Traits> * lazy_string_ptr_;
-		size_t position_;
-	};
+		template <class CharT, class Traits>
+		bool operator!= (CharT const *left, lazy_basic_string<CharT, Traits> const &right) {
+			return right != left;
+		}
+
+		template <class CharT, class Traits>
+		lazy_basic_string<CharT, Traits> operator+ (CharT const *left, lazy_basic_string<CharT, Traits> const &right) {
+			lazy_basic_string<CharT, Traits> result;
+			result += left;
+			result += right;
+			return result;
+		}
+
+		template <class CharT, class Traits>
+		lazy_basic_string<CharT, Traits> operator+ (CharT const left, lazy_basic_string<CharT, Traits> const &right) {
+			lazy_basic_string<CharT, Traits> result;
+			result += left;
+			result += right;
+			return result;
+		}
+
+		template <class CharT, class Traits>
+		void swap(lazy_basic_string<CharT, Traits> &left, lazy_basic_string<CharT, Traits> &right) {
+			left.swap(right);
+		}
+
+		template <class CharT, class Traits = std::char_traits<CharT>>
+		class Proxy {
+		public:
+			Proxy(lazy_basic_string<CharT, Traits> *input_string, size_t idx)
+				:lazy_string_ptr_(input_string), index_(idx)
+			{ }
+
+			operator CharT() {
+				return *(lazy_string_ptr_->c_str() + index_);
+			}
+
+			Proxy& operator= (CharT const &value) {
+				size_t size = lazy_string_ptr_->size();
+
+				std::shared_ptr<CharT[]> new_data = std::make_unique<CharT[]>(size + 1);
+
+				Traits::copy(new_data.get(), lazy_string_ptr_->c_str(), size);
+
+				*(new_data.get() + index_) = value;
+				lazy_string_ptr_->set_data(new_data);
+
+				return *this;
+			}
+
+		private:
+			lazy_basic_string<CharT, Traits> *lazy_string_ptr_;
+			size_t index_;
+		};
+
+		// Структура, обеспечивающая нечувствительность к регистру при сравнении
+		template <class CharT>
+		struct char_itraits : public std::char_traits<CharT> {
+			static int compare(CharT const *string_1, CharT const *string_2, size_t const size) {
+				for (size_t i = 0; i != size; ++i) {
+
+					if (std::toupper(string_1[i]) < std::toupper(string_2[i])) {
+						return -1;
+					}
+
+					if (std::toupper(string_1[i]) > std::toupper(string_2[i])) {
+						return 1;
+					}
+				}
+				return 0;
+			}
+		};
+	} // namespace details
+
+	using namespace details;
 
 	template <class CharT, class Traits = std::char_traits<CharT>>
 	class lazy_basic_string {
  	public:
-
-		lazy_basic_string (std::shared_ptr<CharT[]> data, size_t size)
+		lazy_basic_string (std::shared_ptr<CharT[]> const data, size_t const size)
 			: data_(data), size_(size)
 		{ }
 
 		// Конструктор от другой ленивой строки
-		lazy_basic_string (lazy_basic_string const & c)
-			: data_(c.data_), size_(c.size_)
+		lazy_basic_string (lazy_basic_string const &input_string)
+			: data_(input_string.data_), size_(input_string.size_)
 		{ }
 
 		// Конструктор без параметров
@@ -94,15 +111,15 @@ namespace details {
 		{ }
 
 		// Конструктор неявный от C-строки
-		lazy_basic_string (CharT const * c)	{
-			size_ = Traits::length(c);
+		lazy_basic_string (CharT const *input_string)	{
+			size_ = Traits::length(input_string);
 			data_ = std::make_unique<CharT[]>(size_ + 1);
 
-			Traits::copy(data_.get(), c, size_);
+			Traits::copy(data_.get(), input_string, size_);
 		}
 
 		// Конструктор для заполнения заданным количеством указанных символов
-		lazy_basic_string (size_t n, const CharT & a)
+		lazy_basic_string (size_t const n, CharT const &a)
 			: data_(std::make_unique<CharT[]>(n + 1)), size_(n) {
 
 			Traits::assign(data_.get(), n, a);
@@ -115,14 +132,14 @@ namespace details {
 		}
 
 		// Оператор присваивания
-		lazy_basic_string& operator=(lazy_basic_string const & c) {
-			data_ = c.data_;
-			size_ = c.size();
+		lazy_basic_string& operator= (lazy_basic_string const &input_string) {
+			data_ = input_string.data_;
+			size_ = input_string.size();
 			return *this;
 		}
 
 		// Оператор сложения += для ленивой строки
-		lazy_basic_string & operator+= (const lazy_basic_string & input_string) {
+		lazy_basic_string& operator+= (lazy_basic_string const &input_string) {
 			size_t current_size = size();
 			size_t add_size = input_string.size();
 			size_t new_size = current_size + add_size;
@@ -139,7 +156,7 @@ namespace details {
 		}
 
 		// Оператор сложения += для C-строки
-		lazy_basic_string & operator+= (const CharT * input_string) {
+		lazy_basic_string& operator+= (CharT const *input_string) {
 			size_t current_size = size();
 			size_t add_size = Traits::length(input_string);
 			size_t new_size = current_size + add_size;
@@ -156,7 +173,7 @@ namespace details {
 		}
 
 		// Оператор сложения += для символа
-		lazy_basic_string & operator+= (CharT const input_symbol) {
+		lazy_basic_string& operator+= (CharT const input_symbol) {
 			size_t current_size = size();
 			size_t add_size = 1;
 			size_t new_size = current_size + add_size;
@@ -173,7 +190,7 @@ namespace details {
 		}
 
 		// Оператор сложения + для ленивой строки
-		lazy_basic_string operator+ (const lazy_basic_string & input_string) const {
+		lazy_basic_string operator+ (lazy_basic_string const &input_string) const {
 			size_t current_size = size();
 			size_t add_size = input_string.size();
 			size_t new_size = current_size + add_size;
@@ -187,7 +204,7 @@ namespace details {
 		}
 
 		// Оператор сложения + для C-строки
-		lazy_basic_string operator+ (const CharT * input_string) const {
+		lazy_basic_string operator+ (CharT const *input_string) const {
 			size_t current_size = size();
 			size_t add_size = Traits::length(input_string);
 			size_t new_size = current_size + add_size;
@@ -201,7 +218,7 @@ namespace details {
 		}
 
 		// Оператор сложения + для символа
-		lazy_basic_string operator+ (const CharT input_symbol) const {
+		lazy_basic_string operator+ (CharT const input_symbol) const {
 			size_t current_size = size();
 			size_t add_size = 1;
 			size_t new_size = current_size + add_size;
@@ -215,48 +232,48 @@ namespace details {
 		}
 
 		// Константное обращение по индексу []
-		CharT operator[](size_t i) const {
-			return *(data_.get() + i);
+		CharT operator[] (size_t const idx) const {
+			return *(data_.get() + idx);
 		}
 
 		// Не константное обращение по индексу []
-		Proxy<CharT, Traits> operator[](size_t i) {
-			return Proxy(this, i);
+		Proxy<CharT, Traits> operator[] (size_t const idx) {
+			return Proxy(this, idx);
 		}
 
 		// Сравнение ленивых строк между собой ==
-		bool operator== (const lazy_basic_string & input_string) const {
+		bool operator== (lazy_basic_string const &input_string) const {
 			return Traits::compare(data_.get(), input_string.data_.get(), size()) == 0;
 		}
 
 		// Сравнение ленивых строк с C-строками ==
-		bool operator== (const CharT * input_string) const {
+		bool operator== (CharT const *input_string) const {
 			return Traits::compare(data_.get(), input_string, size()) == 0;
 		}
 
 		// Сравнение ленивых строк между собой !=
-		bool operator!= (const lazy_basic_string & input_string) const {
-			return !operator == (input_string);
+		bool operator!= (lazy_basic_string const &input_string) const {
+			return !(*this == input_string);
 		}
 
 		// Сравнение ленивых строк с C-строками !=
-		bool operator!= (const CharT * input_string) const {
-			return !operator == (input_string);
+		bool operator!= (CharT const *input_string) const {
+			return !(*this == input_string);
 		}
 
 		// Сравнение ленивых строк между собой <
-		bool operator< (const lazy_basic_string & input_string) const {
+		bool operator< (lazy_basic_string const &input_string) const {
 			return Traits::compare(data_.get(), input_string.data_.get(), size()) < 0;
 		}
 
 		// Сравнение ленивых строк с C-строками <
-		bool operator< (const CharT * input_string) const {
+		bool operator< (CharT const *input_string) const {
 			return Traits::compare(data_.get(), input_string, size()) < 0;
 		}
 
 		// Методы, аналогичные std::basic_string
 
-		void swap(lazy_basic_string & input_string) {
+		void swap(lazy_basic_string &input_string) {
 			std::swap(data_, input_string.data_);
 			std::swap(size_, input_string.size_);
 		}
@@ -278,7 +295,7 @@ namespace details {
 			return data_.get();
 		}
 
-		void set_data(std::shared_ptr<CharT[]> data) {
+		void set_data(std::shared_ptr<CharT[]> const data) {
 			data_ = data;
 		}
 
@@ -287,27 +304,9 @@ namespace details {
 		size_t size_;
 	};
 
-	// Структура, обеспечивающая нечувствительность к регистру при сравнении
-	template <class CharT>
-	struct char_itraits : public std::char_traits<CharT> {
-		static int compare(CharT const * string_1, CharT const * string_2, size_t size) {
-			for (size_t i = 0; i != size; ++i) {
-
-				if (std::toupper(string_1[i]) < std::toupper(string_2[i])) {
-					return -1;
-				}
-
-				if (std::toupper(string_1[i]) > std::toupper(string_2[i])) {
-					return 1;
-				}
-			}
-			return 0;
-		}
-	};
-
 	// Определение синонимов
 	typedef lazy_basic_string<char> lazy_string;
 	typedef lazy_basic_string<wchar_t> lazy_wstring;
-	typedef lazy_basic_string<char, char_itraits<char>> lazy_istring;
+	typedef lazy_basic_string<char, details::char_itraits<char>> lazy_istring;
 
 } // namespace std_utils
